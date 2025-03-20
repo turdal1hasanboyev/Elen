adfaff03ca90381b7e72661dae62fbea9abef998
Turdali <turdalihasanboyev70@gmail.com>
2025-03-20 14:49:53 +0500

    Writing contact model

--- apps/contact/admin.py
+++ apps/contact/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Contact
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    """
+    Contact Admin Model
+    """
+
+    model = Contact
+    ordering = ('id',)
+    list_display = (
+        'id',
+        'name',
+        'email',
+        'is_active',
+        'created_at',
+        'updated_at',
+    )
+    search_fields = (
+        'name',
+        'email',
+    )
+    list_filter = (
+        'is_active',
+    )
+    readonly_fields = (
+        'id',
+        'created_at',
+        'updated_at',
+    )
+
+    fieldsets = (
+        ("Basic Information", {
+            "fields": ("name", "email", 'subject', 'message', "is_active",),
+        }),
+        ("Metadata", {
+            "fields": ("id", "created_at", "updated_at",),
+        }),
+    )
--- apps/contact/models.py
+++ apps/contact/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+from apps.common.models import BaseModel
+
+
+class Contact(BaseModel):
+    """
+    Contact model
+    """
+    
+    name = models.CharField(max_length=150, db_index=True)
+    email = models.EmailField(max_length=150, db_index=True, unique=True)
+    subject = models.CharField(max_length=150, null=True, blank=True)
+    message = models.TextField(null=True, blank=True)
+
+    class Meta:
+        """
+        Meta class for Contact model.
+        """
+
+        verbose_name = 'Contact'
+        verbose_name_plural = 'Contacts'
+
+    def __str__(self):
+        """
+        Return a string representation of the model.
+        """
+
+        return f"{self.name}"
+    
